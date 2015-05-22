545d33b180fb84f95b5b075637075757dc1493d4
ashismo <ashishkrmondal@gmail.com>
2015-05-22 22:46:24 +0530

    modified package

--- spring/SpringApplicationContextExample/src/main/java/com/ashish/applicationcontext/MainApp.java
+++ spring/SpringApplicationContextExample/src/main/java/com/ashish/applicationcontext/MainApp.java
@@ -1,4 +1,4 @@
-package com.ashish.beanfactory;
+package com.ashish.applicationcontext;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
