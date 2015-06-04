7872961b8de06c30d57914a6d11dbd20669fb1f0
ashismo <ashishkrmondal@gmail.com>
2015-06-04 22:16:03 +0530

    Projects added for blog
    Projects added for blog

---
+++ maven/ChildJavaProject/src/main/java/com/ashish/servlet/ServletInOtherModule.java
@@ -0,0 +1,21 @@
+package com.ashish.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletInOtherModule extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletInOtherModule() {
+        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("newJSP.jsp");
+	}
+
+}
