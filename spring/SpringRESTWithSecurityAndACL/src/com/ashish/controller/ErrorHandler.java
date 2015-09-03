588da0a8dc94cde64195d4cee06cad75f6f09222
ashismo <ashishkrmondal@gmail.com>
2015-09-03 07:06:39 +0530

    Added SpringRESTWithSecurityAndACL project

---
+++ spring/SpringRESTWithSecurityAndACL/src/com/ashish/controller/ErrorHandler.java
@@ -0,0 +1,21 @@
+package com.ashish.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorHandler implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
---
+++ spring/SpringRESTWithSecurityAndACL/src/com/ashish/main/ApplicationRun.java
@@ -0,0 +1,12 @@
+package com.ashish.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun.class, args);
+    }
+}
