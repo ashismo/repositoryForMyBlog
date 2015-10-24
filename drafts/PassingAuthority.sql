1e3666a6c9bc894b1b9f163872bbc54f234d37cb
ashismo <ashishkrmondal@gmail.com>
2015-10-24 15:26:14 +0530

    Passing authority for Savings
    Passing authority for Savings

--- drafts/PassingAuthority.sql
+++ drafts/PassingAuthority.sql
@@ -1,13 +1,4 @@
-select * into gl_ledger_dtl_bck from gl_ledger_dtl where 1=2;
-alter table gl_ledger_dtl_bck add column operation_flag character varying(2) default NULL;
-select * from gl_ledger_dtl_bck;
-
-select * into gl_ledger_hrd_bck from gl_ledger_hrd where 1=2;
-alter table gl_ledger_hrd_bck add column operation_flag character varying(2) default NULL;
-select * from gl_ledger_hrd_bck;
-
-
---Function to get current financial year
+﻿--Function to get current financial year
 CREATE OR REPLACE FUNCTION getFy ()
 RETURNS char(10) AS $f$
 declare
@@ -23,8 +14,7 @@ BEGIN
    RETURN f;
 END;
 $f$ LANGUAGE plpgsql;
-  
-  
+
 --Trigger to move record into gl_ledger_hrd_bck table
 CREATE OR REPLACE FUNCTION gl_ledger_hrd_passing_auth()
 RETURNS trigger as $$
@@ -34,11 +24,12 @@ BEGIN
     FROM (
 		SELECT count(*) as c
 		FROM gl_ledger_hrd_bck
-	 WHERE tran_id=NEW.tran_id) as s;
-  IF NEW.type in ('sb','jv') and NEW.fy=(select getFy()) and to_char(NEW.action_date,'YYYY-MM-DD')=to_char(now(),'YYYY-MM-DD') -- jv is for bank charges
-	AND recordCnt=0
+	 WHERE tran_id=NEW.tran_id and operation_flag in ('EA')) as s;
+	 
+  IF NEW.type in ('sb','jv') and NEW.fy=(select getFy()) AND recordCnt=0   -- recordCnt = 0 means request is coming from the Application
+	--and to_char(NEW.action_date,'YYYY-MM-DD')=to_char(now(),'YYYY-MM-DD') -- jv is for bank charges
   THEN
-    insert into gl_ledger_hrd_bck select NEW.*,'E';
+    insert into gl_ledger_hrd_bck select NEW.*,null,'E';
     delete from gl_ledger_hrd where tran_id = NEW.tran_id;
   END IF;
   RETURN NEW;
@@ -61,21 +52,25 @@ BEGIN
     FROM (
 		SELECT count(*) as c
 		FROM gl_ledger_dtl_bck
-	 WHERE tran_id=NEW.tran_id) as s;
+	 WHERE tran_id=NEW.tran_id and operation_flag in ('EA')) as s;
 	 
-  IF (NEW.particulars in ('cash','interest') and 
-		NEW.gl_mas_code in ('14101','28101') and NEW.dr_cr in ('Dr','Cr')
-	AND recordCnt in (0,1))
-	OR
-	(NEW.particulars in ('Cleared') and 
-		NEW.gl_mas_code in ('14101') and NEW.dr_cr in ('Cr') 
-	AND recordCnt=0)
-	OR
-	(NEW.particulars in ('bank charge') and 
-		NEW.gl_mas_code in ('28201','14101','57903') and NEW.dr_cr in ('Cr','Dr')  -- 28201: For cheque clear
-	AND recordCnt in (0,1))
+  IF recordCnt = 0 AND   -- recordCnt = 0 means request is coming from Application
+
+	(
+		(NEW.particulars in ('cash','interest') and 
+			NEW.gl_mas_code in ('14101','28101') and NEW.dr_cr in ('Dr','Cr')
+		)
+		OR
+		(NEW.particulars in ('Cleared') and 
+			NEW.gl_mas_code in ('14101') and NEW.dr_cr in ('Cr')
+		)
+		OR
+		(NEW.particulars in ('bank charge') and 
+			NEW.gl_mas_code in ('28201','14101','57903') and NEW.dr_cr in ('Cr','Dr')  -- 28201: For cheque clear
+		)
+	)
   THEN
-    insert into gl_ledger_dtl_bck select NEW.*,'E';
+    insert into gl_ledger_dtl_bck select NEW.*,null,'E';
     delete from gl_ledger_dtl where tran_id = NEW.tran_id;
   END IF;
   RETURN NEW;
@@ -84,7 +79,7 @@ $$LANGUAGE 'plpgsql';
 
 DROP TRIGGER IF EXISTS gl_ledger_dtl_passing_auth_trigger ON gl_ledger_dtl;
 CREATE TRIGGER gl_ledger_dtl_passing_auth_trigger
-AFTER INSERT ON gl_ledger_dtl
+AFTER INSERT OR UPDATE ON gl_ledger_dtl
 FOR EACH ROW
 EXECUTE PROCEDURE gl_ledger_dtl_passing_auth();
 
@@ -95,23 +90,62 @@ ALTER TABLE ONLY gl_ledger_hrd
 
 -- Passing authority function
 CREATE OR REPLACE FUNCTION passing_auth_action(tranId text, paAction text, paComment text)
+RETURNS integer AS $passingStatus$
+
+   Declare commentId integer;
+	   operatorCode char(30);
+	   passingStatus integer;
+BEGIN
+   passingStatus := 1;
+   operatorCode := '';
+   select nextval('comment_seq') into commentId;
+   select operator_code into operatorCode from gl_ledger_hrd_bck where tran_id=tranId; 
+   INSERT INTO comment(id,comments,entry_time,operator_code) values (commentId, paComment, now(), operatorCode);
+   
+  IF paAction in ('ER') THEN   -- E = Entry, ER = Entry then Reject/ D = Deleted, DR = Deleted but Rejected
+    update gl_ledger_hrd_bck set operation_flag=paAction,comment_id=commentId where tran_id=tranId; 
+    update gl_ledger_dtl_bck set operation_flag=paAction,comment_id=commentId where tran_id=tranId;
+  ELSIF paAction in ('EA') THEN
+	PERFORM  gl_ledger_hrd_passing_auth_success(tranId, commentId, paAction, 'UPDATE');
+	PERFORM  gl_ledger_dtl_passing_auth_success(tranId, commentId, paAction, 'UPDATE');
+	
+
+	PERFORM  gl_ledger_hrd_passing_auth_success(tranId, commentId, paAction, 'INSERT');
+	PERFORM  gl_ledger_dtl_passing_auth_success(tranId, commentId, paAction, 'INSERT');
+		
+  END IF;
+   return passingStatus; 
+  exception when others then 
+	return -1;
+END;
+$passingStatus$
+LANGUAGE 'plpgsql';
+
+
+--Manage insert/update in gl_ledger_hrd table
+CREATE OR REPLACE FUNCTION gl_ledger_hrd_passing_auth_success(tranId text, commentId integer, paAction text, operation text)
 RETURNS VOID as $$
 BEGIN
-  -- WRITE A CODE TO INSERT COMMENT INTO comment TABLE and ATTACH the commentId with gl_ledger_hrd_bck and gl_ledger_dtl_bck table
-  IF paAction = 'Reject' THEN
-    update gl_ledger_hrd_bck set operation_flag='ER' where tran_id=tranId;  -- E = Entry, ER = Entry then Reject
-    update gl_ledger_dtl_bck set operation_flag='ER' where tran_id=tranId;
-  ELSIF paAction = 'Accept' THEN
+   IF operation = 'INSERT' THEN
 	insert into gl_ledger_hrd (tran_id,type,action_date, certificate_no, fy,remarks,cheque_no,cheque_dt,operator_code,entry_time)
 	select tran_id,type,action_date, certificate_no, fy,remarks,cheque_no,cheque_dt,operator_code,entry_time from gl_ledger_hrd_bck 
-	where tran_id=tranId and operation_flag  in ('E','D');
+	where tran_id=tranId and operation_flag  in ('EA');
+   ELSIF operation = 'UPDATE' THEN
+	update gl_ledger_hrd_bck set operation_flag=paAction,comment_id=commentId where tran_id = tranId and operation_flag in ('E');
+   END IF;
+END;
+$$LANGUAGE 'plpgsql';
 
-	insert into gl_ledger_dtl (tran_id,gl_mas_code,qty, amount,dr_cr, particulars,account_no)
-	select tran_id,gl_mas_code,qty,amount,dr_cr, particulars,account_no from gl_ledger_dtl_bck 
-	where tran_id=tranId and operation_flag in ('E','D');
-	
-	update gl_ledger_hrd_bck set operation_flag='EA' where tran_id = tranId and operation_flag in ('E','D');
-	update gl_ledger_dtl_bck set operation_flag='EA' where tran_id = tranId and operation_flag in ('E','D');
-  END IF;
+--Manage insert/update in gl_ledger_dtl table
+CREATE OR REPLACE FUNCTION gl_ledger_dtl_passing_auth_success(tranId text, commentId integer, paAction text, operation text)
+RETURNS VOID as $$
+BEGIN
+   IF operation = 'INSERT' THEN
+	insert into gl_ledger_dtl (tran_id,gl_mas_code,qty1, amount,dr_cr, particulars,account_no)
+	select tran_id, gl_mas_code,qty,amount,dr_cr, particulars,account_no from gl_ledger_dtl_bck 
+	where tran_id=tranId and operation_flag in ('EA');
+   ELSIF operation = 'UPDATE' THEN
+	update gl_ledger_dtl_bck set operation_flag=paAction,comment_id=commentId where tran_id = tranId and operation_flag in ('E');
+   END IF;
 END;
 $$LANGUAGE 'plpgsql';
