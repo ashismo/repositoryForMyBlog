caddf08f8291498567299d60bceb244e5336cf71
ashismo <ashishkrmondal@gmail.com>
2016-01-04 23:17:55 +0530

    update 6

---
+++ spring/CustomerSvc/CustomerSvcWS/src/main/java/com/org/coop/config/SpringMvcInitializer.java
@@ -0,0 +1,30 @@
+package com.org.coop.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+ 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+  
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+  
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+ 
+    @Override
+    protected Filter[] getServletFilters() {
+        Filter [] singleton = { new CORSFilter()};
+        return singleton;
+    }
+
+}
