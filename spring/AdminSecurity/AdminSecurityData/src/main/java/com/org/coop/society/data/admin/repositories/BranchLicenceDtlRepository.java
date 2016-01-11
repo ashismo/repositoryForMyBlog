6489600058a15d210df269fa8116530a4649fcd0
ashismo <ashishkrmondal@gmail.com>
2016-01-11 08:51:01 +0530

    update 8

---
+++ spring/AdminSecurity/AdminSecurityData/src/main/java/com/org/coop/society/data/admin/repositories/BranchLicenceDtlRepository.java
@@ -0,0 +1,12 @@
+package com.org.coop.society.data.admin.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.org.coop.society.data.admin.entities.BranchLicenseDtl;
+
+public interface BranchLicenceDtlRepository extends JpaRepository<BranchLicenseDtl, Integer> {
+	public List<BranchLicenseDtl> findByBranchIdOrderByEndDateDesc(int branchId);
+}
