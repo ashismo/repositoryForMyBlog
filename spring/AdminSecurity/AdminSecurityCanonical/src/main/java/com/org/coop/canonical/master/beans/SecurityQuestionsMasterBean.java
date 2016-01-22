6cbcc1c414ea78abb7516de688e32c281e7b6335
ashismo <ashishkrmondal@gmail.com>
2016-01-22 08:52:23 +0530

    update 11

---
+++ spring/AdminSecurity/AdminSecurityCanonical/src/main/java/com/org/coop/canonical/master/beans/SecurityQuestionsMasterBean.java
@@ -0,0 +1,48 @@
+package com.org.coop.canonical.master.beans;
+
+import java.util.Date;
+
+public class SecurityQuestionsMasterBean {
+	private int questionId;
+	private String question;
+	private Date startDate;
+	private Date endDate;
+	private String createUser;
+	private String updateUser;
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+}
