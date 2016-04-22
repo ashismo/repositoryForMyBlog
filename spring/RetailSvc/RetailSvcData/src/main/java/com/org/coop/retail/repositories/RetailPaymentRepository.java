1275c2a64c671e5b9f7682c8cb42d25f4f577dd9
ashismo <ashishkrmondal@gmail.com>
2016-04-22 06:57:52 +0530

    update

---
+++ spring/RetailSvc/RetailSvcData/src/main/java/com/org/coop/retail/repositories/RetailPaymentRepository.java
@@ -0,0 +1,13 @@
+package com.org.coop.retail.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.org.coop.retail.entities.RetailPayment;
+import com.org.coop.retail.entities.RetailTransaction;
+
+public interface RetailPaymentRepository extends JpaRepository<RetailPayment, Integer> {
+	@Query("select rp from RetailPayment rp where rp.transactionPayment.paymentId = :paymentId and rp.materialTranHrd.tranId = :tranId")
+	public RetailPayment findByPaymentIdAndTranId(@Param("paymentId")int paymentId,@Param("tranId")int tranId);
+}
