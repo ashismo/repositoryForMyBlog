9c031d202851b07bd33ec20d5114dc86c3fccde6
ashismo <ashishkrmondal@gmail.com>
2016-05-12 06:49:59 +0530

    update

--- spring/RetailSvc/RetailSvcData/src/main/java/com/org/coop/retail/repositories/RetailStockSellRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.coop.retail.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.coop.retail.entities.StockSell;
-
-public interface RetailStockSellRepository extends JpaRepository<StockSell, Integer> {
-	
-}
