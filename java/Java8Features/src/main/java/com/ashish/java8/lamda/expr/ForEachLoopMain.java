17fffdd52ae2c40e6530e86905fb11e460d76c93
ashismo <ashishkrmondal@gmail.com>
2016-05-28 13:38:54 +0530

    ForEach loop

---
+++ java/Java8Features/src/main/java/com/ashish/java8/lamda/expr/ForEachLoopMain.java
@@ -0,0 +1,25 @@
+package com.ashish.java8.lamda.expr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachLoopMain {
+	public static void main(String args[]) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		
+		System.out.println("Traditional Loop START");
+		for(int i : list) {
+			System.out.println(i);
+		}
+		System.out.println("Traditional Loop END");
+		
+		System.out.println("ForEach Loop START");
+		
+		list.forEach(n -> {System.out.println(n);});
+		
+		System.out.println("ForEach Loop END");
+	}
+}
