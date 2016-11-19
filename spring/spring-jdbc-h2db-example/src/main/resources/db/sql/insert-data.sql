931358bb2bc2740983228dd93557fd4758f4bedb
ashismo <ashishkrmondal@gmail.com>
2016-11-19 20:39:47 +0530

    Deleted unused files

--- spring/spring-jdbc-h2db-example/src/main/resources/db/sql/insert-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username,name,password) VALUES ('ashish', 'Ashish Mondal', 'ashish');
-INSERT INTO users (username,name,password) VALUES ('kumar', 'Ashish Kumar Mondal', 'ashish');
-INSERT INTO users (username,name,password) VALUES ('mondal', 'Ashish Kumar Mondal', 'ashish');
