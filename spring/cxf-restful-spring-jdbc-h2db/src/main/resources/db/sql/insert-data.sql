db4a64c162f20d19beb8ad23c539831c6f9cee9f
Ashish Mondal <ashishkrmondal@gmail.com>
2016-11-24 17:56:38 +0530

    Update insert-data.sql

--- spring/cxf-restful-spring-jdbc-h2db/src/main/resources/db/sql/insert-data.sql
+++ spring/cxf-restful-spring-jdbc-h2db/src/main/resources/db/sql/insert-data.sql
@@ -1,3 +1,7 @@
 INSERT INTO users (username,name,password) VALUES ('ashish', 'Ashish Mondal', 'ashish');
 INSERT INTO users (username,name,password) VALUES ('kumar', 'Ashish Kumar Mondal', 'ashish');
 INSERT INTO users (username,name,password) VALUES ('mondal', 'Ashish Kumar Mondal', 'ashish');
+INSERT INTO role (rolename,role_desc) VALUES ('ADMIN', 'Administrator Role');
+INSERT INTO environment_master (env_name,env_desc) VALUES ('DEV', 'Development');
+INSERT INTO module (module_name,description) VALUES ('iDesk', 'Desktop');
+INSERT INTO module_env (module_id,env_id) VALUES (1, 1);
