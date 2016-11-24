9e17fb1b3dc84784425d7059fdb7bcd62f2624e7
Ashish Mondal <ashishkrmondal@gmail.com>
2016-11-24 18:00:52 +0530

    Update EnvironmentMaster.java

--- spring/cxf-restful-spring-jdbc-h2db/src/main/java/com/ashish/poc/model/EnvironmentMaster.java
+++ spring/cxf-restful-spring-jdbc-h2db/src/main/java/com/ashish/poc/model/EnvironmentMaster.java
@@ -6,6 +6,10 @@ public class EnvironmentMaster {
 	private int envId;
 	private String envName;
 	private String envDesc;
+	private String createUser;
+	private String updateUser;
+	private Timestamp createDate;
+	private Timestamp updateDate;
 	public int getEnvId() {
 		return envId;
 	}
@@ -23,6 +27,30 @@ public class EnvironmentMaster {
 	}
 	public void setEnvDesc(String envDesc) {
 		this.envDesc = envDesc;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+	public Timestamp getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Timestamp updateDate) {
+		this.updateDate = updateDate;
 	}
 
 }
