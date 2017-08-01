54992e5f95c345f32e3246ac831d4cddff50176a
ashismo <ashishkrmondal@gmail.com>
2017-07-31 23:24:36 -0400

    SpringCore example

---
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ashish.learning;
+public class HelloWorld {
+   private String message;
+
+   public void setMessage(String message){
+      this.message  = message;
+   }
+   public void getMessage(){
+      System.out.println("Your Message : " + message);
+   }
+}
---
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/Main.java
@@ -0,0 +1,31 @@
+package com.ashish.learning;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Beans.xml");
+	      
+		// Singleton scope
+		HelloWorld obj1 = (HelloWorld) context.getBean("helloWorld1");
+	    obj1.setMessage("Singleton");
+	    obj1.getMessage();
+
+	    HelloWorld obj2 = (HelloWorld) context.getBean("helloWorld1");
+	    obj2.getMessage();
+	    
+
+	    System.out.println("------------------------------------");
+	    // Prototype scope
+	    HelloWorld obj3 = (HelloWorld) context.getBean("helloWorld2");
+	    obj3.setMessage("Prototype");
+	    obj3.getMessage();
+
+	    HelloWorld obj4 = (HelloWorld) context.getBean("helloWorld2");
+	    obj4.getMessage();
+
+	}
+
+}
