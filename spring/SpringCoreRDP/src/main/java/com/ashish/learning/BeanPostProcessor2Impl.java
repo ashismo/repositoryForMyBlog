ac8ecc27e4b8b5840f65627769833cfc91e6eae4
ashismo <ashishkrmondal@gmail.com>
2017-08-01 23:04:29 -0400

    Bean post processor - order of execution

--- spring/SpringCoreRDP/src/main/java/com/ashish/learning/BeanPostProcessor2Impl.java
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/BeanPostProcessor2Impl.java
@@ -4,7 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.Ordered;
 
-public class BeanPostProcessorImpl implements BeanPostProcessor, Ordered{
+public class BeanPostProcessor2Impl implements BeanPostProcessor, Ordered {
 
 	/**
 	 * This gets executed after initialization of the bean
@@ -14,7 +14,7 @@ public class BeanPostProcessorImpl implements BeanPostProcessor, Ordered{
 	@Override
 	public Object postProcessAfterInitialization(Object arg0, String arg1)
 			throws BeansException {
-		System.out.println("BeanPostProcessorImpl: This gets executed after initialization of the bean: " + arg1);
+		System.out.println("BeanPostProcessor2Impl: This gets executed after initialization of the bean: " + arg1);
 		return arg0;
 	}
 
@@ -26,19 +26,19 @@ public class BeanPostProcessorImpl implements BeanPostProcessor, Ordered{
 	@Override
 	public Object postProcessBeforeInitialization(Object arg0, String arg1)
 			throws BeansException {
-		System.out.println("BeanPostProcessorImpl: This gets executed before initialization of the bean: " + arg1);
+		System.out.println("BeanPostProcessor2Impl: This gets executed before initialization of the bean: " + arg1);
 		return arg0;
 	}
 
 	/**
-	 * Assign the little lower priority to this post processor to ensure 
+	 * Assign the highest priority to this post processor to ensure 
 	 * post processor 2 gets executed before post processor
-	 * return 1 means it has the little lesser priority than 0
+	 * return 0 means it has the highest priority
 	 */
 	@Override
 	public int getOrder() {
 		// TODO Auto-generated method stub
-		return 1;
+		return 0;
 	}
 
 }
