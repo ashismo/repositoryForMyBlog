fc2fa68c1335f03fd126593263ada65b41f6690f
ashismo <ashishkrmondal@gmail.com>
2017-08-04 20:59:52 -0400

    Hybrid configuration. Xml based bean configuration and autowiring is done by annotation

--- spring/SpringCoreRDP/src/main/java/com/ashish/learning/autowire/PhoneManufacturer.java
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/autowire/PhoneManufacturer.java
@@ -1,6 +1,15 @@
 package com.ashish.learning.autowire;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PhoneManufacturer {
+	@Autowired
+	// If qualifier is not added then by default phone bean (i.e. com.ashish.learning.autowire.Nokia) gets injected 
+	// Because, by default, autowiring is by name
+	@Qualifier("phone1")
 	private Phone phone;
 
 	public Phone getPhone() {
