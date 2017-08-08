b1e10e4461639b90d6c453f4efde2b6fe5535ea1
ashismo <ashishkrmondal@gmail.com>
2017-08-08 01:18:57 -0400

    Junit setup

---
+++ spring/SpringCoreRDP/src/test/java/com/ashish/test/SpringUnitTest.java
@@ -0,0 +1,15 @@
+package com.ashish.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:Spring-Beans.xml")
+public class SpringUnitTest {
+	@Test
+	public void DITesting() {
+		System.out.println("Basic Junit setup");
+	}
+}
