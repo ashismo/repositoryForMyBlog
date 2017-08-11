895803156a35c44df12db2bfa4e8735567d22965
Ashish Mondal <ashishkrmondal@gmail.com>
2017-08-11 01:17:14 -0400

    Update Wheels.java

--- spring/SpringCoreRDP/src/main/java/com/ashish/learning/expression/language/Wheels.java
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/expression/language/Wheels.java
@@ -1,5 +1,18 @@
 package com.ashish.learning.expression.language;
 
 public class Wheels {
-
+	private int wheelbase;
+	private String wheelType;
+	public int getWheelbase() {
+		return wheelbase;
+	}
+	public void setWheelbase(int wheelbase) {
+		this.wheelbase = wheelbase;
+	}
+	public String getWheelType() {
+		return wheelType;
+	}
+	public void setWheelType(String wheelType) {
+		this.wheelType = wheelType;
+	}
 }
