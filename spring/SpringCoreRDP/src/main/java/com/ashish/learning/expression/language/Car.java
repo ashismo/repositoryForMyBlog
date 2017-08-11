9555f7b67b92a6bd247a85d1433e8f3b57b1d7fd
Ashish Mondal <ashishkrmondal@gmail.com>
2017-08-11 01:16:15 -0400

    Update Car.java

--- spring/SpringCoreRDP/src/main/java/com/ashish/learning/expression/language/Car.java
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/expression/language/Car.java
@@ -1,5 +1,40 @@
 package com.ashish.learning.expression.language;
 
 public class Car {
-
+	private String wheelType;
+	private String make;
+	private double randomSpeed; 
+	private Wheels wheels;
+	private CarFuel carFuel;
+	
+	public String getWheelType() {
+		return wheelType;
+	}
+	public void setWheelType(String wheelType) {
+		this.wheelType = wheelType;
+	}
+	public Wheels getWheels() {
+		return wheels;
+	}
+	public void setWheels(Wheels wheels) {
+		this.wheels = wheels;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public double getRandomSpeed() {
+		return randomSpeed;
+	}
+	public void setRandomSpeed(double randomSpeed) {
+		this.randomSpeed = randomSpeed;
+	}
+	public CarFuel getCarFuel() {
+		return carFuel;
+	}
+	public void setCarFuel(CarFuel carFuel) {
+		this.carFuel = carFuel;
+	}
 }
