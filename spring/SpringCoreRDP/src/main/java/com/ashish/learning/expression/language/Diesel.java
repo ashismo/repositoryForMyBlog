405e07795252c0d89ee7742d386d523a6edbfbce
Ashish Mondal <ashishkrmondal@gmail.com>
2017-08-11 01:16:44 -0400

    Update Diesel.java

--- spring/SpringCoreRDP/src/main/java/com/ashish/learning/expression/language/Diesel.java
+++ spring/SpringCoreRDP/src/main/java/com/ashish/learning/expression/language/Diesel.java
@@ -1,5 +1,11 @@
 package com.ashish.learning.expression.language;
 
-public class Diesel {
+public class Diesel implements CarFuel {
+
+	@Override
+	public String fuelType() {
+		return "Fuel Type of my car is: Diesel";
+		
+	}
 
 }
