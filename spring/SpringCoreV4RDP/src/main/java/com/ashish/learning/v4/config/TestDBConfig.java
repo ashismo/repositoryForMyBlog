bb4191a40b7941aba01874e6c0e83fc65f7ddeff
ashismo <ashishkrmondal@gmail.com>
2017-08-24 02:01:04 -0400

    Integration with MySql Database->Load initial script

--- spring/SpringCoreV4RDP/src/main/java/com/ashish/learning/v4/config/TestDBConfig.java
+++ spring/SpringCoreV4RDP/src/main/java/com/ashish/learning/v4/config/TestDBConfig.java
@@ -7,31 +7,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import com.ashish.learning.v4.duplicate.beans.DuplicateBean;
 
 @Configuration
-@ComponentScan(basePackages="com.ashish.learning.v4")
-@PropertySource({"classpath:app/properties/database.properties"}) 
+@ComponentScan(basePackages = "com.ashish.learning.v4")
+@PropertySource({ "classpath:app/properties/database.properties" })
 public class TestDBConfig {
-	
+
 	@Autowired
 	private Environment env;
-	
-	
+
 	@Bean
 	@Profile("test")
 	public DriverManagerDataSource datasource() {
 		DriverManagerDataSource datasource = new DriverManagerDataSource();
 		datasource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
 		// If property is not found then the default value would get injected
-		datasource.setUrl(env.getProperty("jdbc.url", "jdbc:mysql://localhost:3306/defaultdb"));
+		datasource.setUrl(env.getProperty("jdbc.url",
+				"jdbc:mysql://localhost:3306/defaultdb"));
 		datasource.setUsername(env.getProperty("jdbc.username", "defaultuser"));
-		datasource.setPassword(env.getProperty("jdbc.password", "defaultpassword"));
+		datasource.setPassword(env.getProperty("jdbc.password",	"defaultpassword"));
+
+		// Initialize the database
+		DatabasePopulatorUtils.execute(createAllTables(), datasource);
 		return datasource;
 	}
-	
+
+	private DatabasePopulator createAllTables() {
+		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+		databasePopulator.setContinueOnError(true);
+
+		databasePopulator.addScript(new ClassPathResource("create-db.sql"));
+		return databasePopulator;
+	}
+
 	@Bean
 	public DuplicateBean dulicateBean() {
 		DuplicateBean duplicateBean = new DuplicateBean();
